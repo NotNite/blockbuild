7daa46cb471280e43b1ce8591e4dabf49c7893d9
NotNite <hi@notnite.com>
2023-06-10 00:46:10 -0400

    some maven stuff, hopefully

--- blockbuild.sh
+++ blockbuild.sh
@@ -9,11 +9,19 @@ else
   rm -rf ./out/*
 fi
 
+if [ ! -d "./out/mvn" ]; then
+  echo "Creating Maven dir..."
+  mkdir ./out/mvn
+fi
+
 hashes_txt_url=$(cat ./host_config.txt)"hashes.txt"
 hashes_status_code=$(curl -s -o /dev/null -w "%{http_code}" $hashes_txt_url)
 
 commits_txt_url=$(cat ./host_config.txt)"commits.txt"
 commits_status_code=$(curl -s -o /dev/null -w "%{http_code}" $commits_txt_url)
+
+out_url=$(cat ./host_config.txt)"out.tar.gz"
+out_status_code=$(curl -s -o /dev/null -w "%{http_code}" $out_url)
 
 previous_commits=""
 previous_hashes=""
@@ -22,21 +30,11 @@ if [ "$hashes_status_code" -eq 200 ] && [ "$commits_status_code" -eq 200 ]; then
   echo "Fetching previous build info..."
   previous_hashes=$(curl -s $hashes_txt_url)
   previous_commits=$(curl -s $commits_txt_url)
+fi
 
-  echo "Redownloading old build artifacts..."
-  IFS=$'\n'
-  for hash_line in $previous_hashes; do
-    file=$(echo $hash_line | cut -d' ' -f3)
-    # Remove up to the first slash
-    file=${file#*/}
-
-    outpath="./out/$file"
-
-    echo "Downloading $file..."
-    mkdir -p $(dirname $outpath)
-    curl -s $(cat ./host_config.txt)$file -o $outpath
-  done
-  unset IFS
+if [ "$out_status_code" -eq 200 ]; then
+  echo "Extracting previous build artifacts..."
+  curl -s $out_url | tar -xz -C ./out
 fi
 
 function build() {
@@ -49,10 +47,12 @@ function build() {
 
   build_dir="$project_arg/build/libs"
   out_dir="../../out/$project_name"
+  mvn_dir=$(pwd -W)/out/mvn
 
   cd ./mods/$project_name
   current_commit=$(git rev-parse HEAD)
-  if [[ "$previous_commits" == *"$current_commit $project_name"* ]]; then
+
+  if [[ "$previous_commits" == *"$current_commit $project_name"* ]] && [ -z "$BLOCKBUILD_FORCE_BUILD" ]; then
     echo "Skipping $project_name as commit hash is unchanged"
     cd ../..
     return
@@ -70,6 +70,20 @@ function build() {
   mkdir -p $out_dir
   cp $build_dir/*.jar $out_dir
 
+  echo "Deploying to Maven..."
+  gradle_properties=$(./gradlew properties -q -p $project_arg)
+
+  for file in $build_dir/*.jar; do
+    mvn deploy:deploy-file \
+    -DgroupId=$(echo "$gradle_properties" | grep "^group:" | cut -d' ' -f2) \
+    -DartifactId=$(echo "$gradle_properties" | grep "^archivesBaseName:" | cut -d' ' -f2) \
+    -Dversion=$(echo "$gradle_properties" | grep "^version:" | cut -d' ' -f2) \
+    -Dpackaging=jar \
+    -DrepositoryId=blockbuild \
+    -Dfile=$file \
+    -Durl=file://$mvn_dir
+  done
+
   cd ../..
 }
 
@@ -83,6 +97,10 @@ for (( i=1; i<=$line_count; i++ )); do
   fi
   build $line
 done
+
+if [ -f "./out.tar.gz" ]; then
+  rm ./out.tar.gz
+fi
 
 echo "Generating hash file..."
 # Append to a temporary file and then move it, so it doesn't appear in the hash list itself
@@ -166,4 +184,9 @@ Expire-Date: 0
   done
 fi
 
-cat ./out/info.txt
+echo "Compressing build artifacts..."
+cd ./out
+tar -czf /tmp/out.tar.gz *
+mv /tmp/out.tar.gz ./out.tar.gz
+
+cat ./info.txt
